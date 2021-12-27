49496e359b0201237ee7e780fad4d6981dfee18e
Fyodor Kavun <kavunfedor01@gmail.com>
2021-12-27 14:18:43 +0200

    Init project and database

---
+++ lab5/SLab5/SLab5/Controllers/IdentityController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SLab5.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class IdentityController
+    {
+        
+    }
+}
